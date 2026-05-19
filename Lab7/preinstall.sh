d3c9a23b4ba62f5e18f8786c9893ca9f18b1902f
fb42dima <dmytfosch-ipt28@lll.kpi.ua>
2026-05-19 18:32:53 +0300

    lab7

---
+++ Lab7/preinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install libopencv-dev cmake gcc g++ wget -y
+
+# Завантажуємо файли нейромережі для детекції облич
+wget -N https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
+wget -N https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel
